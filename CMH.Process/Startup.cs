1edb96714418382c6972381a80c8a7ea2b148192
CitouHub <218735@skane.se>
2022-09-29 16:53:21 +0200

    Revert

--- CMH.Process/Startup.cs
+++ CMH.Process/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 using CMH.Process;
 using CMH.Process.Service;
@@ -20,16 +19,11 @@ namespace CMH.Process
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            var _loggerFactory = new LoggerFactory();
-            var logger = _loggerFactory.CreateLogger<Startup>();
-
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(builder.GetContext().ApplicationRootPath)
                 .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables()
                 .Build();
-
-            builder.Services.AddLogging();
 
             builder.Services.AddAzureClients(_ =>
             {
@@ -46,9 +40,7 @@ namespace CMH.Process
             builder.Services.AddScoped<IRepositoryService, RepositoryService>();
             builder.Services.AddSingleton<ICacheManager, CacheManager>();
 
-            logger.LogInformation("CMH.Process awaiting initialization");
             AwaitInitialization(configuration);
-            logger.LogInformation("CMH.Process started");
         }
 
         static void AwaitInitialization(IConfigurationRoot configuration)
